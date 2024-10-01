bdc56776fd3f7e4e36536715eeaa46d0c2914163
Osama-Brimo <icosahedral.ps@gmail.com>
2024-10-01 15:47:26 +0300

    .

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
-import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
-
-// import { saveSettingsDebounced, callPopup, chat_metadata, executeSlashCommands } from "../../../../script.js";
-
-import {$state} from './index';
 
 const extensionName = "panopticon";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
@@ -14,5 +8,5 @@ const defaultSettings = {};
 // This function is called when the extension is loaded
 jQuery(async () => {
   console.log(`[${extensionName}] loaded`);
-  console.log(`[${extensionName}] Game State`, $state);
+  // console.log(`[${extensionName}] Game State`, $state);
 });

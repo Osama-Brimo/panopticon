e69472e92128340bd8abc9752a3b88c3098bfec6
Osama-Brimo <icosahedral.ps@gmail.com>
2024-10-01 15:41:17 +0300

    .

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 
-import { saveSettingsDebounced, callPopup, chat_metadata, executeSlashCommands } from "../../../../script.js";
+// import { saveSettingsDebounced, callPopup, chat_metadata, executeSlashCommands } from "../../../../script.js";
 
 import {$state} from './index';
 
-const extensionName = "Panopticon";
+const extensionName = "panopticon";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {};

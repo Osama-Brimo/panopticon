ba81f6627f404f625f8c748ce81e1be1ce14f5b7
Osama-Brimo <icosahedral.ps@gmail.com>
2024-10-01 20:29:10 +0300

    .

--- src/classes/Ables/Talkable.js
+++ src/classes/Ables/Talkable.js
@@ -2,6 +2,7 @@ import { Assistant } from "../ST/Assistant.js";
 
 export class Talkable extends Assistant {
     constructor(name, description, on_talk) {
+        super(model);
         this.name = name;
         this.description = description;
         this.on_talk = on_talk;

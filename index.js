d0ca3902d50a365f9cd24c29f8326d4a5e4a8c3e
Osama-Brimo <icosahedral.ps@gmail.com>
2024-10-01 20:44:04 +0300

    .

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ import { executeSlashCommandsWithOptions } from '../../../slash-commands.js';
 
 const extensionName = "panopticon";
 
+
+executeSlashCommandsWithOptions(`/setvar key="foobartest_a" ${$state.area.name}`); 
+
+
 // This function is called when the extension is loaded
 jQuery(async () => {
   try {
     console.log(`[${extensionName}] loaded`);
     console.log(`[${extensionName}] Game State`, $state);
-    executeSlashCommandsWithOptions(`/setvar key="foobartest" ${$state.area.name}`); 
+    executeSlashCommandsWithOptions(`/setvar key="foobartest_b" ${$state.area.name}`); 
   } catch (error) {
     console.error('panopticon error', error);
   }
+
+  executeSlashCommandsWithOptions(`/setvar key="foobartest_c" ${$state.area.name}`);
+  
+  executeSlashCommandsWithOptions(`/setglobalvar key="foobartest_d" ${$state.area.name}`); 
+
 });

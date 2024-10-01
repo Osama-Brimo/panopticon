73868e9131cfb742de57626fe24ecf8e3ea644e9
Osama-Brimo <icosahedral.ps@gmail.com>
2024-10-01 15:52:05 +0300

    .

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
+import {$state} from './src/test';
 
 const extensionName = "panopticon";
-const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-const extensionSettings = extension_settings[extensionName];
-const defaultSettings = {};
-
 
 // This function is called when the extension is loaded
 jQuery(async () => {
   console.log(`[${extensionName}] loaded`);
-  // console.log(`[${extensionName}] Game State`, $state);
+  console.log(`[${extensionName}] Game State`, $state);
 });
